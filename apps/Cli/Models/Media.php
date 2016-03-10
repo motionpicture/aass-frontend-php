056f0fc68efc33135f7ce53ff06f1352fbb81c34
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-03-10 17:06:34 +0900

    CLIアプリをmysqlに対応。

--- apps/Cli/Models/Media.php
+++ apps/Cli/Models/Media.php
@@ -1,76 +1,41 @@
 <?php
 namespace Aass\Cli\Models;
 
-use \WindowsAzure\Table\Models\EdmType;
-use \WindowsAzure\Table\Models\Entity;
-use \WindowsAzure\Table\Models\Filters\Filter;
-
 class Media extends \Aass\Common\Models\Media
 {
     public function getByStatus($status)
     {
-        $entities = [];
+        $statement = $this->db->prepare('SELECT id, title, status, filename, extension, asset_id, job_id, job_state FROM media WHERE status = :status');
+        $statement->execute([
+            'status' => $status
+        ]);
 
-        $qopts = new \WindowsAzure\Table\Models\QueryEntitiesOptions();
-        $filter = Filter::applyEq(Filter::applyPropertyName('Status'), Filter::applyConstant("{$status}", EdmType::STRING));
-        $qopts->setFilter($filter);
-        $qopts->setTop(1);
-        $result = $this->azureTable->queryEntities('Media', $qopts);
-        $entities = $result->getEntities();
-
-        return (!empty($entities)) ? $entities[0] : null;
+        return $statement->fetch();
     }
 
-    public function updateAsset($entity, $assetId)
+    public function updateJob($id, $jobId, $jobState)
     {
-        $entity = $this->addExtraInfo($entity);
-        $entity->addProperty('AssetId', EdmType::STRING, $assetId);
-        $entity->addProperty('Status', EdmType::STRING, self::STATUS_ASSET_CREATED);
+        $statement = $this->db->prepare('UPDATE media SET status = :status, job_id = :jobId, job_state = :jobState, updated_at = NOW() WHERE id = :id');
+        $result = $statement->execute([
+            ':id' => $id,
+            ':jobId' => $jobId,
+            ':jobState' => $jobState,
+            ':status' => self::STATUS_JOB_CREATED
+        ]);
 
-        $this->azureTable->mergeEntity('Media', $entity);
+        return $result;
     }
 
-    public function addExtraInfo($mediaEntity)
+    public function updateJobState($id, $state, $url, $status)
     {
-        $path = self::getUploadedFilePath($mediaEntity);
+        $statement = $this->db->prepare('UPDATE media SET url = :url, job_state = :jobState, status = :status, updated_at = NOW() WHERE id = :id');
+        $result = $statement->execute([
+            ':id' => $id,
+            ':url' => $url,
+            ':jobState' => $state,
+            ':status' => $status
+        ]);
 
-        $size = (filesize($path)) ? filesize($path) : '';
-        $mediaEntity->addProperty('Size', EdmType::STRING, "{$size}");
-
-        // 再生時間を取得
-        $getID3 = new \getID3;
-        $fileInfo = $getID3->analyze($path);
-        $this->logger->addInfo('getID3->analyze $fileInfo:' . var_export($fileInfo, true));
-        if (isset($fileInfo['playtime_string'])) {
-            $mediaEntity->addProperty('PlaytimeString', EdmType::STRING, "{$fileInfo['playtime_string']}");
-        }
-        if (isset($fileInfo['playtime_seconds'])) {
-            $mediaEntity->addProperty('PlaytimeSeconds', EdmType::STRING, "{$fileInfo['playtime_seconds']}");
-        }
-
-        return $mediaEntity;
-    }
-
-    public function updateJob($entity, $jobId, $jobState)
-    {
-        $entity->addProperty('JobId', EdmType::STRING, "{$jobId}");
-        $entity->addProperty('JobState', EdmType::STRING, "{$jobState}");
-        $entity->addProperty('Status', EdmType::STRING, self::STATUS_JOB_CREATED);
-
-        $this->azureTable->mergeEntity('Media', $entity);
-    }
-
-    public function updateJobState($entity, $state, $url, $status)
-    {
-        $entity->addProperty('JobState', EdmType::STRING, "{$state}");
-        $entity->addProperty('Url', EdmType::STRING, "{$url}");
-        $entity->addProperty('Status', EdmType::STRING, "{$status}");
-        $this->azureTable->mergeEntity('Media', $entity);
-    }
-
-    public function updateStatus($entity, $status)
-    {
-        $entity->addProperty('Status', EdmType::STRING, "{$status}");
-        $this->azureTable->mergeEntity('Media', $entity);
+        return $result;
     }
 }
--- apps/Cli/Tasks/MediaTask.php
+++ apps/Cli/Tasks/MediaTask.php
@@ -15,184 +15,29 @@ ini_set('memory_limit', '1024M');
 class MediaTask extends BaseTask
 {
     /**
-     * アップロードされたファイルに対して処理を施す
-     *
-     * 1.メディアサーバーへアップロード
-     * 2.メディアエンティティを更新
-     */
-    public function local2assetAction()
-    {
-        $mediaEntity = null;
-        $assetId = null;
-        $isComleted = false;
-
-        try {
-            // アップロード済みのメディアをひとつ取得
-            $mediaModel = new MediaModel;
-            $mediaEntity = $mediaModel->getByStatus(MediaModel::STATUS_UPLOADED);
-            $this->logger->addInfo("uploaded media:" . var_export($mediaEntity, true));
-        } catch (\Exception $e) {
-            $this->logger->addError("fail in getByStatus {$e}");
-        }
-
-        if (!is_null($mediaEntity)) {
-            try {
-                // メディアサービスへ資産としてアップロードする
-                list($assetId, $isComleted) = $this->ingestAsset($mediaEntity);
-                $this->logger->addInfo("ingestAsset Success. assetId:{$assetId}");
-                if (!$isComleted) {
-                    throw new \Exception('ingestAsset not completed.');
-                }
-            } catch (\Exception $e) {
-                $this->logger->addError("fail in ingestAsset {$e}");
-            }
-
-            if (!is_null($assetId) && $isComleted) {
-                try {
-                    // メディアエンティティ更新
-                    $mediaModel->updateAsset($mediaEntity, $assetId);
-                    $this->logger->addInfo("assetId has been added to media entity.");
-                } catch (\Exception $e) {
-                    $this->logger->addError("fail in updateAsset. message:{$e}");
-                }
-            }
-
-            if ($isComleted) {
-                // ストレージへのアップロード完了していれば元ファイル削除
-                if ($this->mode != 'dev') {
-                    unlink(MediaModel::getUploadedFilePath($mediaEntity));
-                }
-            } else {
-                if (!is_null($assetId)) {
-                    // TODO アセット削除？
-                }
-
-                // エラーステータスに更新
-                $mediaModel->updateStatus($mediaEntity, MediaModel::STATUS_ERROR);
-            }
-        }
-    }
-
-    /**
-     * 資産をインジェストする
-     *
-     * @see http://msdn.microsoft.com/ja-jp/library/jj129593.aspx
-     * @return array
-     */
-    private function ingestAsset($mediaEntity)
-    {
-        $asset = null;
-        $assetId = null;
-        $isCompleted = false;
-
-        try {
-            // 資産を作成する
-            $asset = new Asset(Asset::OPTIONS_NONE);
-            $asset->setName($mediaEntity->getRowKey());
-            $asset = $this->mediaService->createAsset($asset);
-            $assetId = $asset->getId();
-            $this->logger->addInfo("asset has been created. asset:{$assetId}");
-        } catch (\Exception $e) {
-            $this->logger->addError('createAsset throw exception. message:' . $e->getMessage());
-        }
-
-        if (!is_null($assetId)) {
-            $isUploaded = false;
-            try {
-                // ファイルのアップロードを実行する
-                $fileName = "{$mediaEntity->getRowKey()}.{$mediaEntity->getPropertyValue('Extension')}";
-                $path = MediaModel::getUploadedFilePath($mediaEntity);
-                $this->logger->addInfo("creating BlockBlob... path:{$path}");
-                $content = fopen($path, 'rb');
-                $result = $this->blobService->createBlockBlob(basename($asset->getUri()), $fileName, $content);
-                $this->logger->addDebug('BlockBlob has been created. result:' . var_export($result, true));
-                fclose($content);
-
-                $isUploaded = true;
-            } catch (\Exception $e) {
-                $this->logger->addError("upload2storage throw exception. message:{$e}");
-            }
-            $this->logger->addInfo('isUploaded:' . var_export($isUploaded, true));
-
-            if ($isUploaded) {
-                try {
-                    // ファイル メタデータの生成
-                    $this->mediaService->createFileInfos($asset);
-
-                    // ここまできて初めて、アセットの準備が完了したことになる
-                    $isCompleted = true;
-                    $this->logger->addInfo("inputAsset has been prepared completely. asset:{$assetId}");
-                } catch (\Exception $e) {
-                   $this->logger->addError("createFileInfos throw exception. message:{$e}");
-                }
-            }
-        }
-
-        return [$assetId, $isCompleted];
-    }
-
-    /**
-     * アップロードディレクトリに残ったゴミファイルを物理削除する
-     */
-    public function cleanAction()
-    {
-        $dir = __DIR__ . '/../../../uploads';
-        $iterator = new \RecursiveDirectoryIterator($dir);
-        $iterator = new \RecursiveIteratorIterator($iterator);
-
-        $files4delete = [];
-        foreach ($iterator as $fileinfo) {
-            try {
-                // $fileinfoはSplFiIeInfoオブジェクト
-                // ドット始まりのファイルは除外
-                if ($fileinfo->isFile() && substr($fileinfo->getFilename(), 0, 1) != '.') {
-                    // 3日以上経過していれば追加
-                    $absence = time() - $fileinfo->getCTime();
-                    if ($absence > 60 * 60 * 24 * 3) {
-                        $files4delete[] = [
-                            'path'  => $fileinfo->getPathname(),
-                            'ctime' => date('Y-m-d H:i:s', $fileinfo->getCTime()) // inode変更時刻
-                        ];
-                    }
-                }
-            } catch (Exception $e) {
-                $this->logger->log('fileinfo-> throw exception. message:' . $e->getMessage());
-            }
-        }
-
-        $this->logger->log('files4delete:' . count($files4delete));
-
-        // 削除
-        foreach ($files4delete as $file) {
-            unlink($file['path']);
-            $this->logger->log("A file has been deleted. path:{$file['path']}");
-        }
-    }
-
-    /**
      * エンコード処理を施す
      */
     public function encodeAction()
     {
-        $mediaEntity = null;
+        $media = null;
         $job = null;
         $isUpdated = false;
 
         try {
             // アセット作成済みのメディアエンティティを取得
             $mediaModel = new MediaModel;
-            $mediaEntity = $mediaModel->getByStatus(MediaModel::STATUS_ASSET_CREATED);
-            $this->logger->addInfo("media:" . var_export($mediaEntity, true));
+            $media = $mediaModel->getByStatus(MediaModel::STATUS_ASSET_CREATED);
+            $this->logger->addInfo("media:" . var_export($media, true));
         } catch (\Exception $e) {
-            $this->logger->addError("fail in getByStatus {$e}");
+            $this->logger->addError("mediaModel->getByStatus throw exception. message:{$e}");
         }
 
-        if (!is_null($mediaEntity)) {
+        if ($media) {
             try {
-                $job = $this->createJob($mediaEntity);
+                $job = $this->createJob($media);
 
                 if (!is_null($job)) {
-                    $mediaModel->updateJob($mediaEntity, $job->getId(), $job->getState());
+                    $mediaModel->updateJob($media['id'], $job->getId(), $job->getState());
                     $isUpdated = true;
                 }
             } catch (\Exception $e) {
@@ -208,7 +53,7 @@ class MediaTask extends BaseTask
      *
      * @return \WindowsAzure\MediaServices\Models\Job
      */
-    private function createJob($mediaEntity)
+    private function createJob($media)
     {
         $job = null;
 
@@ -220,7 +65,7 @@ class MediaTask extends BaseTask
             'JobInputAsset(0)',
             'JobOutputAsset(0)',
             Asset::OPTIONS_NONE,
-            "{$mediaEntity->getRowKey()}[adaptive_bitrate_mp4]"
+            "{$media['id']}[adaptive_bitrate_mp4]"
         );
         $task = new Task($taskBody, $mediaProcessor->getId(), TaskOptions::NONE);
 //         $task->setConfiguration('H264 Adaptive Bitrate MP4 Set 1080p');
@@ -231,10 +76,10 @@ class MediaTask extends BaseTask
         $tasks[] = $task;
         $this->logger->addInfo('tasks has been prepared. tasks count:' . count($tasks));
 
-        $inputAsset = $this->mediaService->getAsset($mediaEntity->getPropertyValue('AssetId'));
+        $inputAsset = $this->mediaService->getAsset($media['asset_id']);
 
         $job = new Job();
-        $job->setName("Aass_job_for_{$mediaEntity->getRowKey()}");
+        $job->setName("Aass_job_for_{$media['id']}");
         $job = $this->mediaService->createJob($job, [$inputAsset], $tasks);
 
         $this->logger->addInfo("job has been created. job:" . var_export($job, true));
@@ -260,28 +105,28 @@ class MediaTask extends BaseTask
      */
     public function checkJobAction()
     {
-        $mediaEntity = null;
+        $media = null;
         $job = null;
 
         try {
             // ジョブ作成済みのメディアエンティティを取得
             $mediaModel = new MediaModel;
-            $mediaEntity = $mediaModel->getByStatus(MediaModel::STATUS_JOB_CREATED);
-            $this->logger->addInfo("media:" . var_export($mediaEntity, true));
+            $media = $mediaModel->getByStatus(MediaModel::STATUS_JOB_CREATED);
+            $this->logger->addInfo("media:" . var_export($media, true));
         } catch (\Exception $e) {
             $this->logger->addError("fail in getByStatus {$e}");
         }
 
-        if (!is_null($mediaEntity)) {
+        if ($media) {
             try {
                 // メディアサービスよりジョブを取得
-                $job = $this->mediaService->getJob($mediaEntity->getPropertyValue('JobId'));
+                $job = $this->mediaService->getJob($media['job_id']);
             } catch (\Exception $e) {
                 $this->logger->addError("mediaServicesWrapper->getJob() throw exception. message:{$e}");
             }
 
             // ジョブのステータスを更新
-            if (!is_null($job) && $mediaEntity->getPropertyValue('JobState') != $job->getState()) {
+            if (!is_null($job) && $media['job_state'] != $job->getState()) {
                 $state = $job->getState();
                 $this->logger->addInfo("job state change. new state:{$state}");
                 try {
@@ -290,19 +135,19 @@ class MediaTask extends BaseTask
                         // ジョブのアウトプットアセットを取得
                         $assets = $this->mediaService->getJobOutputMediaAssets($job->getId());
                         $asset = $assets[0];
-                        $url = $this->createUrl($asset->getId(), $asset->getName(), $mediaEntity->getRowKey());
+                        $url = $this->createUrl($asset->getId(), $media['filename']);
 
                         // ジョブに関する情報更新と、URL更新
-                        $mediaModel->updateJobState($mediaEntity, $state, $url, MediaModel::STATUS_ENCODED);
+                        $mediaModel->updateJobState($media['id'], $state, $url, MediaModel::STATUS_ENCODED);
 
                         // TODO URL通知
 //                         if (!is_null($url)) {
 //                             $this->sendEmail($media);
 //                         }
                     } else if ($state == Job::STATE_ERROR || $state == Job::STATE_CANCELED) {
-                        $mediaModel->updateJobState($mediaEntity, $state, '', MediaModel::STATUS_ERROR);
+                        $mediaModel->updateJobState($media['id'], $state, '', MediaModel::STATUS_ERROR);
                     } else {
-                        $mediaModel->updateJobState($mediaEntity, $state, '', MediaModel::STATUS_JOB_CREATED);
+                        $mediaModel->updateJobState($media['id'], $state, '', MediaModel::STATUS_JOB_CREATED);
                     }
                 } catch (\Exception $e) {
                     $this->logger->addError("delivering url for streaming throw exception. message:{$e}");
@@ -316,11 +161,10 @@ class MediaTask extends BaseTask
      * @see http://msdn.microsoft.com/ja-jp/library/jj889436.aspx
      *
      * @param string $assetId
-     * @param string $assetName
-     * @param string $rowKey
+     * @param string $filename
      * @return string
      */
-    private function createUrl($assetId, $assetName, $rowKey)
+    private function createUrl($assetId, $filename)
     {
         // 特定のAssetに対して、同時に5つを超える一意のLocatorを関連付けることはできない
         // 万が一OnDemandOriginロケーターがあれば削除
@@ -345,7 +189,7 @@ class MediaTask extends BaseTask
         $locator = $this->mediaService->createLocator($locator);
 
         // URLを生成
-        $url = sprintf('%s%s.ism/Manifest', $locator->getPath(), $rowKey);
+        $url = "{$locator->getPath()}{$filename}.ism/Manifest";
         $this->logger->addInfo("urls have been created. url:{$url}");
 
         return $url;
