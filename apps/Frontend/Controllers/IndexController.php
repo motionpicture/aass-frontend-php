2f37a8a656cc66f985d8ae830cecb6b95e1a341b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-03-29 17:19:58 +0900

    AASS-7 スケールアウト時の挙動確認。

--- apps/Frontend/Controllers/IndexController.php
+++ apps/Frontend/Controllers/IndexController.php
@@ -8,4 +8,10 @@ class IndexController extends BaseController
         $this->logger->addDebug($this->mode);
         $this->response->redirect('medias');
     }
-}
+
+    public function scaleOutTestAction()
+    {
+        $this->logger->addDebug("Server IP address:{$_SERVER['LOCAL_ADDR']}");
+        echo "Server IP address:{$_SERVER['LOCAL_ADDR']}";
+        exit;
+    }}
